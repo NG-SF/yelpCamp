ddc19f0244edbef96a61174daac6e878126cc03c
Nataliya Gapochka <natasha.gapochka@gmail.com>
2017-12-28 18:09:28 -0800

    Add show route

--- app.js
+++ app.js
@@ -1,44 +1,30 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require("mongoose");
 
-let campgrounds = [{
-        name: "Creek camp",
-        image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"
-    },
-    {
-        name: "River camp",
-        image: "https://farm9.staticflickr.com/8577/16263386718_c019b13f77.jpg"
-    },
-    {
-        name: "Mountain camp",
-        image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"
-    },
-    {
-        name: "Creek camp",
-        image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"
-    },
-    {
-        name: "River camp",
-        image: "https://farm9.staticflickr.com/8577/16263386718_c019b13f77.jpg"
-    },
-    {
-        name: "Mountain camp",
-        image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"
-    },
-    {
-        name: "Creek camp",
-        image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"
-    },
-    {
-        name: "River camp",
-        image: "https://farm9.staticflickr.com/8577/16263386718_c019b13f77.jpg"
-    },
-    {
-        name: "Mountain camp",
-        image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"
-    }
-];
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+//schema setup
+let camproundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+let Campground = mongoose.model("Campground", camproundSchema);
+
+// Campground.create({
+//     name: "Mountain camp",
+//     image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg",
+//     description: "This is a beautiful campground, with magnificent views of mountains, great facilities, huge parking lot. Come and experience nature at its best."
+// }, function (err, camp) {
+//     if (err) {
+//         console.log("error happened", err);
+//     } else {
+//         console.log("camp added", camp);
+//     }
+// });
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -49,9 +35,15 @@ app.get('/', function (req, res) {
     res.render("landing");
 });
 
+//get all campgrounds from the DB
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {
-        campgrounds: campgrounds
+    Campground.find({}, function (err, camps) {
+        if (err) {
+            console.log("error happened", err);
+        } else {
+            console.log("Here are all the Campground");
+            res.render("index", { Campground: camps });
+        }
     });
 });
 
@@ -59,17 +51,34 @@ app.post("/campgrounds", function (req, res) {
     //get data from form and add to campgrounds array
     let name = req.body.name;
     let image = req.body.image;
+    let description = req.body.description;
     let newCampground = {
         name: name,
-        image: image
+        image: image,
+        description: description
     };
-    campgrounds.push(newCampground);
-    //redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    // create a new campground and save to DB
+    Campground.create(newCampground, function (err, newCamp) {
+        if (err) {
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function (req, res) {
     res.render("new.ejs");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCamp){
+        if(err) { console.log(err); }
+        else { //render show template with that campground
+        res.render("show", { Campground: foundCamp });
+        }
+    });
 });
 
 app.listen(process.env.PORT || 3000, function () {
