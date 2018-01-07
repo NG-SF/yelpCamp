87d0bcc466388345164bb6cf33f4154d5791f8cd
Nataliya Gapochka <natasha.gapochka@gmail.com>
2018-01-06 20:05:53 -0800

    add public stylesheet

--- seeds.js
+++ seeds.js
@@ -7,17 +7,17 @@ mongoose.Promise = global.Promise;
 let data = [{
   name: "Mountain camp",
   image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg",
-  description: "This is a beautiful campground, with magnificent views of mountains, great facilities, huge parking lot. Come and experience nature at its best."
+  description: "This is a beautiful campground, with magnificent views of mountains, great facilities, huge parking lot. Come and experience nature at its best.The pull-right class is now float-right in Bootstrap 4... It's also better to not align the ul list and use block elements for the rows. Use button tag instead of input and use pull-right class. pull-right class totally messes up both of your buttons, but you can fix this by defining custom margin on the right side."
 },
 {
   name: "Lone grove",
   image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg",
-  description: "This is a beautiful campground, with magnificent views of mountains, great facilities, huge parking lot. Come and experience nature at its best."
+  description: "This is a beautiful campground, with magnificent views of mountains, great facilities, huge parking lot. Come and experience nature at its best. The pull-right class is now float-right in Bootstrap 4... It's also better to not align the ul list and use block elements for the rows. Use button tag instead of input and use pull-right class. pull-right class totally messes up both of your buttons, but you can fix this by defining custom margin on the right side."
 },
 {
   name: "Valley camp",
   image: "https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg",
-  description: "This is a beautiful campground, with magnificent views of mountains, great facilities, huge parking lot. Come and experience nature at its best."
+  description: "This is a beautiful campground, with magnificent views of mountains, great facilities, huge parking lot. Come and experience nature at its best. The pull-right class is now float-right in Bootstrap 4... It's also better to not align the ul list and use block elements for the rows. Use button tag instead of input and use pull-right class. pull-right class totally messes up both of your buttons, but you can fix this by defining custom margin on the right side."
 }];
 
 function seedDB() {
